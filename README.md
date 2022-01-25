02c45e0af1d60ec0a8781d95275b58daddd72a25
rangerhu <675786480@qq.com>
2022-01-25 18:53:07 +0800

    ouro auto compound auto pool中一旦有用户的外部操作,立即执行pool中所有用户的compound操作

--- README.md
+++ README.md
@@ -1,39 +1,19 @@
 # Ouro Auto Compound
 
-#### 介绍
-{**以下是 Gitee 平台说明，您可以替换此简介**
-Gitee 是 OSCHINA 推出的基于 Git 的代码托管平台（同时支持 SVN）。专为开发者提供稳定、高效、安全的云端软件开发协作平台
-无论是个人、团队、或是企业，都能够用 Gitee 实现代码托管、项目管理、协作开发。企业项目请看 [https://gitee.com/enterprises](https://gitee.com/enterprises)}
+#### 初版介绍
+1. 目前的auto compound是质押ogs，赚取ogs
 
-#### 软件架构
-软件架构说明
+2. assetContract即质押资产固定为ogs，由此Ouro_Auto_ogs合约设定为不需要构造函数
 
+3. 由于solidity的特性，关于什么时候把用户的利息拿去compound(即用户积攒到何时的利息再拿去投资)，必须得有外部触发机制
 
-#### 安装教程
+   初版的auto compound定义为:
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+   **auto ogs pool中，每当有用户执行withdraw或deposit操作，清算并当前pool中所有用户，并触发当前pool中所有用户的compound**
 
-#### 使用说明
+   *(注:compound的效果与auto ogs池的社区用户参与活跃程度正相关,极端情况当auto ogs池后续没有活跃社区用户时,无compound效果,即与manual的效果相同)*
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+4. 当用户执行deposit操作后，用户资金押入pool中开启auto compound；当用户执行deposi操作时，结算用户目前的所有收益
 
-#### 参与贡献
+5. 因为用户在执行外部操作时将进行pool中所有用户的清算并执行所有用户的compound，由是claimRewards没有任何意义，返回值始终为0，即用户的利息已经变为质押的本金，如果要取出，因为采用withdraw来取出质押的本金利息
 
-1.  Fork 本仓库
-2.  新建 Feat_xxx 分支
-3.  提交代码
-4.  新建 Pull Request
-
-
-#### 特技
-
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
--- contracts/Ouro_Auto_ogs.sol
+++ contracts/Ouro_Auto_ogs.sol
@@ -31,7 +31,6 @@ contract LPStaking is Ownable, ReentrancyGuard,Pausable {
    
     //==改动开始==
     uint256 private _numStaker = 0 ; // track the number of total staker
-    mapping (address => uint256) private _stakersAddressIndex; // tracking stakers' address index 
     mapping (uint256 => address) private _stakersAddress; // tracking stakers' address  
     //==改动结束==
 
@@ -100,18 +99,17 @@ contract LPStaking is Ownable, ReentrancyGuard,Pausable {
     /**
      * @dev stake assets
      */
-    //清算当前存款用户的rewards，compound其他所有用户的rewards
+    //清算并compound所有用户
     function deposit(uint256 amount) external nonReentrant whenNotPaused {
         require(amount > 0, "zero deposit");
-        // settle previous rewards
-        settleStaker(msg.sender);
+        // settle and compound all rewards
+        settleStaker();
         
         // modifiy
         _balances[msg.sender] += amount;
         _totalStaked += amount;
 
         //==改动开始==
-        _stakersAddressIndex[msg.sender] = _numStaker; //the first staker start with index value 0
         _stakersAddress[_numStaker] = msg.sender; //the index 0 mapping to the first staker address
         _numStaker += 1; //add a staker
         
@@ -124,34 +122,38 @@ contract LPStaking is Ownable, ReentrancyGuard,Pausable {
         emit Deposit(msg.sender, amount);
     }
     
-    /**
-     * @dev claim rewards
-     */
-    //清算当前拿取rewards用户的rewards，compound其他所有用户的rewards
-    function claimRewards() external nonReentrant whenNotPaused {
-        // settle previous rewards
-        settleStaker(msg.sender);
+    //==改动开始==
+    //在结算时将自动compound所有用户，所以claimRewards触发时中所有用户reward的值固定为0，即_rewardBalance[msg.sender]=0
+    //用户的所有利息与本金均应直接通过 withdraw取出，即用户的所有利息与本金在结算时均在_balances[msg.sender]中
+    // /**
+    //  * @dev claim rewards
+    //  */
+    // //清算并compound所有用户
+    // function claimRewards() external nonReentrant whenNotPaused {
+    //     // settle and compound all rewards
+    //     settleStaker();
         
-        // reward balance modification
-        uint amountReward = _rewardBalance[msg.sender];
-        delete _rewardBalance[msg.sender]; // zero reward balance
+    //     // reward balance modification
+    //     uint amountReward = _rewardBalance[msg.sender];
+    //     delete _rewardBalance[msg.sender]; // zero reward balance
 
-        // mint reward to sender
-        IOGSToken(ogsContract).mint(msg.sender, amountReward);
+    //     // mint reward to sender
+    //     IOGSToken(ogsContract).mint(msg.sender, amountReward);
         
-        // log
-        emit OGSClaimed(msg.sender, amountReward);
-    }
+    //     // log
+    //     emit OGSClaimed(msg.sender, amountReward);
+    // }
+    //==改动结束==
     
     /**
      * @dev withdraw the staked assets
      */
-    //清算当前取款balance用户的rewards，compound其他所有用户的rewards
+    //清算并compound所有用户
     function withdraw(uint256 amount) external nonReentrant {
         require(amount <= _balances[msg.sender], "balance exceeded");
 
-        // settle previous rewards
-        settleStaker(msg.sender);
+        // settle and compound all rewards
+        settleStaker();
 
         // modifiy
         _balances[msg.sender] -= amount;
@@ -169,66 +171,51 @@ contract LPStaking is Ownable, ReentrancyGuard,Pausable {
      */
     //==改动开始==
     //关于什么时候把用户的利息拿去compound，必须得有触发机制，即用户积攒到何时的利息再拿去投资？
-    //***这里暂时定义为当前auto ogs池中每当有其他人来withdraw或deposit或claimRewards时触发用户利息的复利(注:compound的效果与auto ogs池的社区用户参与活跃程度正相关,极端情况当auto ogs池后续没有活跃社区用户时,无compound效果,即与manual的效果相同)***
-    //每当结算一个用户的时候,自动地将其他所有用户的利润放到质押池中,清零其他所有用户的利润,更新其他所有用户的质押金额与总质押池的大小
-    function settleStaker(address account) internal {
+    //***这里暂时定义为当前auto ogs池中每当有用户来withdraw或deposit或claimRewards时触发所有用户的compound(注:compound的效果与auto ogs池的社区用户参与活跃程度正相关,极端情况当auto ogs池后续没有活跃社区用户时,无compound效果,即与manual的效果相同)***
+    //每当结算一个用户的时候,自动地将所有用户的利润放到质押池中,清零所有用户的利润,更新所有用户的质押金额与总质押池的大小
+    function settleStaker() internal {
         // update reward snapshot
         updateReward();
         
         // settle this account
-        uint accountCollateral = _balances[account];
-        uint lastSettledRound = _settledRounds[account];
+        uint accountCollateral;
+        uint lastSettledRound;
         uint newSettledRound = _currentRound - 1;
         
         // round rewards
-        uint roundReward = _accShares[newSettledRound].sub(_accShares[lastSettledRound])
-                                .mul(accountCollateral)
-                                .div(SHARE_MULTIPLIER);  // remember to div by SHARE_MULTIPLIER    
+        uint roundReward;  
         
-        // update reward balance
-        _rewardBalance[account] += roundReward;
-
-        // mark new settled reward round
-        _settledRounds[account] = newSettledRound;
-        
-
         ///===改动开始===
-        uint256 accountIndex = _stakersAddressIndex[account]; //获取当前结算用户的Index
-
-        // reinvest all accounts'reward except this settle staker
+        // reinvest all accounts'reward
         //由于balances即质押的是ogs,reward也是ogs,所以这里应该可以直接相加
-        if (_numStaker > 1){     //当auto ogs池中参与人数为两位或以上时,才有其他staker的 auto compound
-            // auto compound 除开当前结算用户的其他所有用户
+        if (_numStaker > 0){     //auto ogs池必须有参与用户
+            // auto compound 所有用户
             for (uint256 _i=0; _i<_numStaker; _i++ ){
+                   
+                address currentStaker = _stakersAddress[_i]; //当前池中其他Staker的地址
 
-                if (_i != accountIndex){
-                    
-                    address currentStaker = _stakersAddress[_i]; //当前池中其他Staker的地址
+                // settle this account
+                accountCollateral = _balances[currentStaker];
+                lastSettledRound = _settledRounds[currentStaker];
 
-                    // settle this account
-                    accountCollateral = _balances[currentStaker];
-                    lastSettledRound = _settledRounds[currentStaker];
+                // round rewards
+                roundReward = _accShares[newSettledRound].sub(_accShares[lastSettledRound])
+                                .mul(accountCollateral)
+                                .div(SHARE_MULTIPLIER);  // remember to div by SHARE_MULTIPLIER
 
-                    // round rewards
-                    roundReward = _accShares[newSettledRound].sub(_accShares[lastSettledRound])
-                                    .mul(accountCollateral)
-                                    .div(SHARE_MULTIPLIER);  // remember to div by SHARE_MULTIPLIER
+                // update reward balance
+                _rewardBalance[currentStaker] += roundReward;
+                
+                // mark new settled reward round
+                _settledRounds[currentStaker] = newSettledRound;
 
-                    // update reward balance
-                    _rewardBalance[currentStaker] += roundReward;
-                    
-                    // mark new settled reward round
-                    _settledRounds[currentStaker] = newSettledRound;
-
-                    //reinvest current account's reward
-                    _balances[currentStaker] += _rewardBalance[currentStaker];
-                    _totalStaked += _rewardBalance[currentStaker];
-                    // log current account reinvest
-                    emit Deposit(currentStaker, _rewardBalance[currentStaker]);
-                    // zero reward balance, save gas
-                    delete _rewardBalance[currentStaker];  //如果测试报错的话,该行改为:  _rewardBalance[currentStaker]=0  
-
-                }
+                //reinvest current account's reward
+                _balances[currentStaker] += _rewardBalance[currentStaker];
+                _totalStaked += _rewardBalance[currentStaker];
+                // log current account reinvest
+                emit Deposit(currentStaker, _rewardBalance[currentStaker]);
+                // zero reward balance, save gas
+                delete _rewardBalance[currentStaker];  //如果测试报错的话,该行改为:  _rewardBalance[currentStaker]=0  
 
             }
         }
